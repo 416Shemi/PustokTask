3ccc334b60e08a0e6bffb75295288a710d1fdb48
Shamsaddin Amanov <tu7ngcsvr@aztu.local>
2022-05-23 17:51:59 +0400

    new project

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,14 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.DAL;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Slider> sliders = _context.Sliders.ToList();
+            return View(sliders);
         }
     }
 }
